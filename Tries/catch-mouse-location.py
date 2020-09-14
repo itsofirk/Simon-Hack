1443de0d572ba4d4ebb96c292a235dfc5475dd51
Tetro <itsofirk@gmail.com>
2020-09-15 00:28:19 +0300

    Ables to capture mouse location

--- Tries/catch-mouse-location.py
+++ Tries/catch-mouse-location.py
@@ -1,5 +1,24 @@
+import threading
 
+import win32gui
+
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+
+query_mouse_position = win32gui.GetCursorPos
 
 if __name__ == '__main__':
     print("Press Ctrl-K when mouse is placed")
+    print("Capturing now...")
+
+    set_interval(query_mouse_position, 0.1)
+
     print("Exiting...")
